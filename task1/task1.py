3df94e8ae120226fe353dc188f3938c493e537b5
Svyatoslav2 <20020511@bk.ru>
2026-07-22 16:23:09 +0300

    task2

--- task1/task1.py
+++ task1/task1.py
@@ -1,4 +1,6 @@
-n, m = map(int,input().split())
+import sys
+n = int(sys.argv[1])
+m = int(sys.argv[2])
 test=[]
 arr = list(range(1, n + 1))
 result="1"
@@ -10,4 +12,3 @@ while True:
     result += str(arr[end])       
     current = end     
 print(result)
-
--- task2/task2.py
+++ task2/task2.py
@@ -1,1 +1,30 @@
+import sys
+first_file=sys.argv[1]
+second_file=sys.argv[2]
+eps = 1e-9
+with open(first_file, 'r') as file:
+    centre = file.readline().strip()
+    radius = file.readline().strip()
 
+xc,xy = map(float,centre.split())
+radius=float(radius)
+coords=[]
+with open(second_file, 'r') as file:
+    while True:
+
+        temp=file.readline().strip()
+        if temp:
+            bx,by=map(float,temp.split())
+            coords.append([bx,by])
+        else:
+            break
+
+
+for i in coords:
+    diff = (i[0]-xc)**2 + (i[1]-xy)**2 - radius**2
+    if abs(diff) < eps:
+        print(0)
+    elif diff>0:
+        print(2)
+    else:
+        print(1)
