f710bfb09023361c3cacbaa3cf14f7870508a2cb
Svyatoslav2 <20020511@bk.ru>
2026-07-22 18:22:24 +0300

    task4

--- task4/task4.py
+++ task4/task4.py
@@ -1,1 +1,21 @@
+import sys
+first_file=sys.argv[1]
+numbers=[]
+with open(first_file,'r') as f:
+    while True:
+        
+        temp=f.readline().strip()
+        if temp: 
+            numbers.append(int(temp))
+        else:
+            break
+numbers=sorted(numbers)
+medianna= numbers[len(numbers)//2]
+result=0
+for i in numbers:
+    diff=i-medianna
+    result+=abs(diff)
+
+print(result)
+
 
