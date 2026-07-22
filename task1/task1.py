9ed8dca5cc5a6951815c6d78dbefe3a5ee70a1d7
Svyatoslav2 <20020511@bk.ru>
2026-07-22 15:37:22 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,1 +1,13 @@
+n, m = map(int,input().split())
+test=[]
+arr = list(range(1, n + 1))
+result="1"
+current=0
+while True:
+    end = (current + m - 1) % n   
+    if end == 0:                 
+        break
+    result += str(arr[end])       
+    current = end     
+print(result)
 
